7d4409db42f5dc8476596430006e950f86b966c1
Venus Kawaii <venusxnew@gmail.com>
2025-02-12 02:53:38 -0700

    Delete launch_game.bat

--- launch_game.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Cambiar a la ruta donde se encuentra el flashplayer_32_sa.exe
-cd FPSoftware\Flash
-REM Ejecutar el flashplayer_32_sa.exe con la ruta al archivo SWF
-flashplayer_32_sa.exe "..\..\assets\angry-birds-valentines-day-7100.swf"
-pause
